5c096b061cb87ba776a830748a17679e3a11a025
Chris Chang <chris@crchang.com>
2024-12-02 01:52:36 -0800

    Add files via upload

---
+++ SEED Library Tools/code/cfg.py
@@ -0,0 +1,8 @@
+DATA_DIR = '../data/'
+OUT_DIR = '../out/'
+SEQ_DIR = DATA_DIR + 'sequencing/'
+OLIGO_DIR = DATA_DIR + 'oligo_pools/'
+CODON_FREQ_INPUT_FILE = DATA_DIR + 'hs_codon_freq.csv'
+
+LIBRARY_CONSTANT_U = 'TTCGGTGTCAGGTCCAGTTT' #Constant sequence upstream of the library
+LIBRARY_CONSTANT_D = 'ATTCTCCTGGGGAAGGCCAC' #Constant sequence downstream of the library
